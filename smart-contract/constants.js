db5255fdbb8b91fe501f0bad12ad730eed806445
Sunthosh <heysunthosh@gmail.com>
2022-02-05 17:18:04 +0530

    Added code for DAO Dapp

---
+++ smart-contract/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0xaC1c085bb86Fc38732882561CBc1f0Df22A1F477";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
